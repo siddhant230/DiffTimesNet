c752d139449b2e72e2e74dd0761df04659f1ad03
siddhant230 <rsiddhant73@gmail.com>
2024-01-27 12:19:15 +0530

    normal  attention across period

--- models/DiffTimesNet/src/model/siren_model/layer.py
+++ models/DiffTimesNet/src/model/siren_model/layer.py
@@ -47,54 +47,61 @@ class SirenLayer(nn.Module):
 
 
 class Attention(nn.Module):
-    def __init__(self, dims, n_heads, qkv_bias=False,
-                 attention_drop_p=0.5, projection_drop_p=0.5):
+    def __init__(self, dim, hidden_dim=16, n_heads=12, qkv_bias=True, attn_p=0., proj_p=0.):
         super().__init__()
-
-        self.dims = dims
         self.n_heads = n_heads
-        self.qkv_bias = qkv_bias
-        self.attention_drop_p = attention_drop_p
-        self.projection_drop_p = projection_drop_p
-
-        self.head_dim = dims // n_heads
+        self.dim = hidden_dim
+        self.head_dim = hidden_dim // n_heads
         self.scale = self.head_dim ** -0.5
-        self.qkv = nn.Linear(self.dims, self.dims*3, bias=self.qkv_bias)
-        self.attention_dropout = nn.Dropout(attention_drop_p)
-        self.projection = nn.Linear(self.dims, self.dims)
-        self.projection_dropout = nn.Dropout(projection_drop_p)
+
+        self.project0 = nn.Linear(dim, hidden_dim)
+        self.qkv = nn.Linear(hidden_dim, hidden_dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_p)
+        self.proj = nn.Linear(hidden_dim, dim)
+        self.proj_drop = nn.Dropout(proj_p)
 
     def forward(self, x):
-        n_samples, n_tokens, dims = x.shape
+        x = self.project0(x)
+        n_samples, n_tokens, dim = x.shape
+        if dim != self.dim:
+            raise ValueError
 
-        if dims != self.dims:
-            raise ValueError("dimensions mismatch")
-        qkv = self.qkv(x)
-        # for qkv
-        qkv = qkv.reshape(n_samples, n_tokens, 3,
-                          self.n_heads, self.head_dim
-                          ).permute(2, 0, 3, 1, 4)
-        query, key, value = qkv[0], qkv[1], qkv[2]
-        dot_prouct_q_k = (query @ key.transpose(-2, -1)) * self.scale
-        attention = dot_prouct_q_k.softmax(dim=-1)
-        attention = self.attention_dropout(attention)
-        context = attention @ value
-        context = context.transpose(1, 2).flatten(2)
-        out = self.projection(context)
-        out = self.projection_dropout(out)
-        return out
+        qkv = self.qkv(x)  # (n_samples, n_patches + 1, 3 * dim)
+        qkv = qkv.reshape(
+            n_samples, n_tokens, 3, self.n_heads, self.head_dim
+        )  # (n_smaples, n_patches + 1, 3, n_heads, head_dim)
+        qkv = qkv.permute(
+            2, 0, 3, 1, 4
+        )
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        # (n_samples, n_heads, head_dim, n_patches + 1)
+        k_t = k.transpose(-2, -1)
+        dp = (
+            q @ k_t
+        ) * self.scale  # (n_samples, n_heads, n_patches + 1, n_patches + 1)
+        # (n_samples, n_heads, n_patches + 1, n_patches + 1)
+        attn = dp.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        weighted_avg = attn @ v
+        weighted_avg = weighted_avg.transpose(
+            1, 2)
+        weighted_avg = weighted_avg.flatten(2)
+        x = self.proj(weighted_avg)
+        x = self.proj_drop(x)
+        return x
 
 
 class BlockAttention(nn.Module):
     def __init__(self, x_dim):
         super().__init__()
-        self.attn_block_period = Attention(x_dim[0], n_heads=1)
-        self.attn_block_ftrs = Attention(x_dim[1], n_heads=1)
+        self.attn_block_period = Attention(x_dim[0], n_heads=2)
+        # self.attn_block_ftrs = Attention(x_dim[1], n_heads=2)
         self.alpha = torch.nn.Parameter(torch.randn((1, 1)))
-        self.beta = torch.nn.Parameter(torch.randn((1, 1)))
+        # self.beta = torch.nn.Parameter(torch.randn((1, 1)))
 
     def forward(self, x):
         x_p = self.attn_block_period(x.permute(0, 2, 1)).permute(0, 2, 1)
-        x_ftrs = self.attn_block_ftrs(x)
-        x_out = self.alpha*x_p + self.beta*x_ftrs
+        # x_ftrs = self.attn_block_ftrs(x)
+        x_out = self.alpha*x_p  # + self.beta*x_ftrs
         return x_out
